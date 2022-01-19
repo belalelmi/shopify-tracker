8905e392fb3f08a930c6e42cb3e4229131e5b4fe
belalelmi <belalelmi@gmail.com>
2022-01-19 03:35:44 -0500

    removed unnecessary cookie-parser

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-const createError = require('http-errors');
-const express = require('express');
+// load config.env data into process.env
 const dotenv = require('dotenv')
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./server/routes/index');
-const usersRouter = require('./server/routes/users');
-
-const app = express();
-
 dotenv.config({ path: 'config.env' })
+
+// Web server config
+const express = require('express');
+const path = require('path');
+const app = express();
+const logger = require('morgan');
+const createError = require('http-errors');
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+app.use(logger('dev'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+const indexRouter = require('./src/routes/index');
+const itemsRouter = require('./src/routes/items');
+const updateInventoryRouter = require('./src/routes/update_inventory');
+const exportDataRouter = require('./src/routes/export_data')
+
+app.use('/', indexRouter(db));
+app.use('/add_item', itemsRouter(db));
+app.use('/update_inventory', updateInventoryRouter(db));
+app.use('/export_data', exportDataRouter(db))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -31,14 +42,11 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 
