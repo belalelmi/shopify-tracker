ed1fc778c253af512c410a8d81180beeeb95d2a7
belalelmi <belalelmi@gmail.com>
2022-01-19 02:54:22 -0500

    add initial seeds for database

---
+++ src/db/seeds/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO inventory (name, quantity, description, price, bin, certification)
+VALUES
+('Mud Tank', 22, 'Solid control system mud tank', 13800, 'VX 326', 'ISO 900'),
+('Kelly Hose', 70, 'High pressure pump hose', 3499, 'AP 32', 'ISO 1020'),
+('Drill Line', 400, 'Pipe wire line drill, high quality', 90, 'AE 901', 'RXO 920b'),
+('Travelling Block', 104, 'pulley drilling block', 4990, 'C4A 09', 'ISO 900'),
+('Well Drilling Bit', 48, '', 1800, 'BA 03', 'TISO 800'),
+('Tricon Roller', 35, 'Tricon roller drill bit', 1500, 'EW 912', 'BXO 712'),
+('Bore Hole Drill', 8, 'Deep hole drill', 46700, 'YK 61', 'RXO 920b'),
+('Drawworks RG', 18, 'Drawworks - SUN 901 certified', 17000, 'IT 08', 'SUN 0901');
